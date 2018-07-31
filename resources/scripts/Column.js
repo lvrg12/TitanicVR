2d7e5cf18de54f7537251dc960a40e0c79b9c572
lvrg12 <lino.virgen96@gmail.com>
2018-07-31 10:52:13 -0500

    Update Column.js

--- resources/scripts/Column.js
+++ resources/scripts/Column.js
@@ -37,6 +37,7 @@ function Column( coord, values, len, colors, attributes, group)
         var geometry = new THREE.CylinderGeometry( len/50, len/50, value, 32 );
         var cylinder = new THREE.Mesh( geometry, new THREE.MeshBasicMaterial( { color: color } ) );
         cylinder.position.set( x, tempTopValue+value/2 + y, z );
+        if( HIVE & x==0 & y==0 ) cylinder.rotation.z = Math.PI/2;
         cylinder.material.transparent = true;
         cylinder.attributes = attributes;
         group.add( cylinder );
