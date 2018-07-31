96532b2ac1c8b10b0105b0bbeddbd5df9baec61b
lvrg12 <lino.virgen96@gmail.com>
2018-07-31 10:05:26 -0500

    Added y to markerLocation

--- resources/scripts/GridHive.js
+++ resources/scripts/GridHive.js
@@ -12,13 +12,8 @@ function GridHive( columns, len, fieldNames, optionNames, group)
     var geometry;
 
     // drawing quadrants
-    for( var i=0; i<columns.length; i++)
-    {
-        if( i == 1 )
-            continue;
-        
+    for( var i=0; i<columns.length; i++)        
         addQuad( 2*Math.PI/(columns.length), i );
-    }
 
     // drawing text
     for( var f=0; f<columns.length; f++)
@@ -34,14 +29,15 @@ function GridHive( columns, len, fieldNames, optionNames, group)
         var t = -2 * Math.PI/columns.length * field;
         var x = len/5 + len + len/5;
 
-        addText(text, [ x * Math.cos(t) , x * Math.sin(t) ], true);
+        addText(text, [ x * Math.cos(t) , 0, x * Math.sin(t) ], true);
     }
 
     function addText( text, coord, isFieldName )
     {
         var xR = ( isFieldName )? -2*Math.PI/4 : 0;
-        var z = coord[1];
-        var x = coord[0]
+        var x = coord[0];
+        var y = coord[1];
+        var z = coord[2];
 
         var loader = new THREE.FontLoader();
 
@@ -72,7 +68,8 @@ function GridHive( columns, len, fieldNames, optionNames, group)
     function addMarker( coord )
     {
         var x = coord[0];
-        var z = coord[1];
+        var y = coord[1];
+        var z = coord[2];
 
         geometry = new THREE.Geometry();
         geometry.vertices.push(new THREE.Vector3( x, 0, z ));
@@ -81,11 +78,21 @@ function GridHive( columns, len, fieldNames, optionNames, group)
         group.add( marker );
     }
 
-    function addQuad( angle ,q )
+    function addQuad( angle, q )
     {
         geometry = new THREE.Geometry();
-        geometry.vertices.push(new THREE.Vector3( len/5, 0, 0 ));
-        geometry.vertices.push(new THREE.Vector3( len+len/5, 0, 0 ));
+        
+        if( q != 1 )
+        {
+            geometry.vertices.push(new THREE.Vector3( len/5, 0, 0 ));
+            geometry.vertices.push(new THREE.Vector3( len+len/5, 0, 0 ));
+        }
+        else // draw important field line
+        {
+            geometry.vertices.push(new THREE.Vector3( 0, len/5, 0 ));
+            geometry.vertices.push(new THREE.Vector3( 0, len+len/5, 0 ));
+        }
+        
         var quad = new THREE.Line( geometry, material.clone() );
         quad.rotation.set( 0, q * angle, 0);
         group.add( quad );
@@ -99,11 +106,15 @@ function GridHive( columns, len, fieldNames, optionNames, group)
 
         if( option > num_of_options-1 || option < 0 )
             console.error("Field " + field + " does not contain option " + option);
+        
         var separation = len / ( num_of_options - 1 );
         var t = -2 * Math.PI/columns.length * field;
         var x = len/5 + option * separation;
         
-        return [ x * Math.cos(t) , x * Math.sin(t) ];
+        if( field != 1 )
+            return [ x * Math.cos(t), 0, x * Math.sin(t) ];
+        else // return important marker location
+            return [ 0, len/5 + option * separation, 0 ];
     }
 
     function getFieldCount()
@@ -114,5 +125,4 @@ function GridHive( columns, len, fieldNames, optionNames, group)
     this.markerLocation = markerLocation;
     this.getFieldCount = getFieldCount;
 
-    
 }
