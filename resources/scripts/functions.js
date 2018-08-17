69ef63fc1c4cf30fed932080b6f7d0b0bbc6e7f8
Guest User <guestuser@Vinhs-iMac.local>
2018-08-17 16:26:14 -0500

    navigation update

--- resources/scripts/functions.js
+++ resources/scripts/functions.js
@@ -288,7 +288,7 @@ function onDocumentMouseDown( event )
         else
         {
             if( VR )
-                TIMER = setInterval(function(){camera.translateZ( -10 );}, 10);
+                TIMER = setInterval(function(){camera.translateZ( -10 );}, 1);
                 // camera.translateZ(-100)
         }
         FILTERED = 0;
