d18157b7c402d0366018fe1e93821bf92d0616d0
Lino <lino.virgen96@gmail.com>
2018-09-04 14:31:50 -0500

    vr nav update

--- resources/scripts/functions.js
+++ resources/scripts/functions.js
@@ -35,8 +35,9 @@ function init()
 
     window.requestAnimationFrame( render );
     window.addEventListener( 'mousedown', onDocumentMouseDown, false );
-    window.addEventListener( 'touchstart', onDocumentTouchStart, false );
-    window.addEventListener( 'touchend', onDocumentTouchEnd, false )
+    // window.addEventListener( 'touchstart', onDocumentTouchStart, false );
+    // window.addEventListener( 'touchend', onDocumentTouchEnd, false )
+    window.addEventListener( 'keypressed', onkeypress, false )
     window.addEventListener( 'resize', onWindowResize, false );
 
     //document.body.appendChild( WEBVR.createButton( renderer ) );
@@ -346,6 +347,12 @@ function onDocumentMouseDown( event )
         }
         FILTERED = 0;
     }
+}
+
+function onkeypress( event )
+{
+    if( VR )
+        camara.translateZ(-10);
 }
 
 function onDocumentTouchStart( event )
