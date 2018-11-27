be6875a42ab9c9c1d17cb8692e6feee53d67d8a2
Lino <lino.virgen96@gmail.com>
2018-11-27 17:43:12 -0600

    controller trial

--- resources/scripts/functions.js
+++ resources/scripts/functions.js
@@ -1,3 +1,12 @@
+AFRAME.registerComponent('x-button-listener', {
+    init: function () {
+      var el = this.el;
+      el.addEventListener('xbuttondown', function (evt) {
+        el.setAttribute('visible', !el.getAttribute('visible'));
+      });
+    }
+  });
+
 function init()
 {
 
