980f099bb3cb70473dc17fe37aaa1c75e663d3e0
Lino <lino.virgen96@gmail.com>
2018-11-27 17:49:23 -0600

    laser

--- resources/scripts/functions.js
+++ resources/scripts/functions.js
@@ -1,7 +1,7 @@
-AFRAME.registerComponent('x-button-listener', {
+AFRAME.registerComponent('a-button-listener', {
     init: function () {
       var el = this.el;
-      el.addEventListener('xbuttondown', function (evt) {
+      el.addEventListener('abuttondown', function (evt) {
         el.setAttribute('visible', !el.getAttribute('visible'));
       });
     }
