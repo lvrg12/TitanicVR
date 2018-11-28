96d2c13593c9151d6eb38f28868407fcbf600739
Lino <lino.virgen96@gmail.com>
2018-11-28 13:51:25 -0600

    boxes added

--- resources/scripts/functions.js
+++ resources/scripts/functions.js
@@ -1,3 +1,26 @@
+AFRAME.registerComponent('boxes', {
+    init: function () {
+      var box;
+      var columns = 20;
+      var el = this.el;
+      var i;
+      var j;
+      var rows = 15;
+      if (el.sceneEl.isMobile) {
+        columns = 10;
+        rows = 5;
+      };
+      for (x = columns / -2; x < columns / 2; x++) {
+        for (y = 0.5; y < rows; y++) {
+          box = document.createElement('a-entity');
+          box.setAttribute('mixin', 'box');
+          box.setAttribute('position', {x: x * .6, y: y * .6, z: 1.5});
+          el.appendChild(box);
+        }
+      }
+    }
+  });
+
 function init()
 {
 
